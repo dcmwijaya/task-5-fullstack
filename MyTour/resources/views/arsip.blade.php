121a616ad43d4c664207ea790ca5a0c76410187e
devancakra <mudrawijaya@gmail.com>
2022-11-16 04:30:50 +0700

    Fix: View Article

--- MyTour/resources/views/arsip.blade.php
+++ MyTour/resources/views/arsip.blade.php
@@ -80,18 +80,31 @@
 </div>
 
 @foreach($data as $v)
-    <div class="modal fade modalmenu" id="#ModalViewArticle-{{ $v->id }}" tabindex="-1" aria-labelledby="ModalViewArticle" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header bg-success text-light">
-                    <h5 class="modal-title"> Tampilan Artikel</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Test-{{ $v->id }}
-                </div>
+<div class="modal fade modalmenu" id="ModalViewArticle-{{ $v->id }}" tabindex="-1" aria-labelledby="ModalViewArticleLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-secondary text-light">
+                <strong><p class="modal-title" id="ModalViewArticleLabel"><label for="judulpostingform" class="col-form-label">
+                    <i class="bi bi-bookmarks-fill me-1"></i>
+                    {{ $v->title }} @if (($v->category_id) == '1') {{ "(Kategori: Khusus)" }} @elseif (($v->category_id) == '2') {{ "(Kategori: Umum)" }} @endif
+                </label></p></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <div class="modal-body"><br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{ $v->image }}" class="img-fluid rounded-start img-profile" alt="gambarpengguna" style="height:240px;max-height:auto;width:240px;max-width:auto;">
+                    </div>
+                    <div class="col-md-6 pb-3">
+                        <label for="contentform" class="col-form-label"><i class="fa-regular fa-comment-dots me-1"></i>{{ __('Komentar Anda') }}</label>
+                        <textarea type="text" class="form-control" id="contentform" rows="8" cols="66" style="text-align:justify;white-space:normal;" readonly disabled>
+                            {{ $v->content }}
+                        </textarea>
+                    </div>
+                </div>
+            </div>  
         </div>
     </div>
+</div>
 @endforeach
 @endsection

8354bef08bcf42191f5b41a2e21245144df49bc5
devancakra <mudrawijaya@gmail.com>
2022-11-16 06:22:33 +0700

    Fix: Update Article

--- MyTour/resources/views/arsip.blade.php
+++ MyTour/resources/views/arsip.blade.php
@@ -28,6 +28,14 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div><br>
             @endif
+            @if ($SuccessUpdateArticle = Session::get('updateArticle'))
+                <div class="alert alert-success alert-dismissible fade show m-3 mt-3 md-3" role="alert">
+                    <small class="text-muted"><i class="bi bi-info-square-fill me-1"></i>
+                        {{ $SuccessUpdateArticle }}
+                    </small>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div><br>
+            @endif
             @if ($SuccessDeleteArticle = Session::get('deleteArticle'))
                 <div class="alert alert-success alert-dismissible fade show m-3 mt-3 md-3" role="alert">
                     <small class="text-muted"><i class="bi bi-info-square-fill me-1"></i>
@@ -75,7 +83,7 @@
 
 @foreach($data as $v)
 <div class="modal fade modalmenu" id="ModalViewArticle-{{ $v->id }}" tabindex="-1" aria-labelledby="ModalViewArticleLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog" style="min-width:1000px;min-height:500px;align-items:center;margin-top:100px;">
         <div class="modal-content">
             <div class="modal-header bg-secondary text-light">
                 <strong><p class="modal-title" id="ModalViewArticleLabel"><label for="judulpostingform" class="col-form-label">
@@ -87,7 +95,7 @@
             <div class="modal-body"><br>
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="{{ $v->image }}" class="img-fluid rounded-start img-profile" alt="gambarpengguna" style="height:240px;max-height:auto;width:240px;max-width:auto;">
+                        <img src="{{ $v->image }}" class="img-fluid rounded-start img-profile" alt="gambartour" style="height:240px;max-height:auto;width:450px;max-width:auto;">
                     </div>
                     <div class="col-md-6 pb-3">
                         <label for="contentform" class="col-form-label"><i class="fa-regular fa-comment-dots me-1"></i>{{ __('Komentar Anda') }}</label>
@@ -100,6 +108,57 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade modalmenu" id="ModalUpdateArticle-{{ $v->id }}" tabindex="-1" aria-labelledby="ModalUpdateArticleLabel" aria-hidden="true">
+        <div class="modal-dialog" style="min-width:1000px;min-height:500px;align-items:center;">
+            <div class="modal-content">
+                <div class="modal-header bg-secondary text-light">
+                    <strong><p class="modal-title" id="ModalUpdateArticleLabel">
+                        <i class="fa-solid fa-book-open-reader me-1"></i> Ubah Artikel
+                    </p></strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="row g-2" action="{{ url('updatearticle/'.$v->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row col-md-12">
+                            <div class="col-md-6 mt-2">
+                                <label for="imgpostingform" class="col-form-label"><i class="bi bi-card-image me-1"></i>{{ __('Foto Tour Lama') }}</label>
+                                <img src="{{ $v->image }}" class="img-fluid rounded-start img-profile" alt="gambartour" style="height:240px;max-height:auto;width:450px;max-width:auto;">
+                                <label for="imgpostingform" class="col-form-label mt-2"><i class="bi bi-card-image me-1"></i>{{ __('Ubah Foto Tour Baru') }}</label>
+                                <div class="input-group mb-3 mt-2">
+                                    <input type="file" class="form-control" name="image" id="imgpostingform" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mt-2">
+                                <div class="col-md-12 mt-2">
+                                    <label for="judulpostingform" class="col-form-label"><i class="bi bi-bookmarks-fill me-1"></i>{{ __('Ubah Judul Postingan') }}</label>
+                                    <input type="text" class="form-control mt-2" name="title" id="judulpostingform" value="{{ $v->title }}" placeholder="Ubah Judul..." required>
+                                </div>
+                                <div class="col-md-12 mt-2">
+                                    <label for="categoryform" class="col-form-label"><i class="fa-solid fa-circle-question me-1"></i>{{ __('Ubah Jenis Posting') }}</label>
+                                    <select id="categoryform" name="category_id" class="form-select g-3 mb-3 mt-2" aria-label=".form-select-lg example">
+                                        <option value="2" class="text-small" selected>Umum</option>
+                                        <option value="1" class="text-small">Khusus</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-12 mt-2">
+                                    <label for="contentform" class="col-form-label"><i class="fa-regular fa-comment-dots me-1"></i>{{ __('Ubah Komentar Anda') }}</label>
+                                    <textarea type="text" class="form-control mt-2" name="content" id="contentform" cols="30" rows="5" style="text-align:justify;white-space:normal;" placeholder="Ubah Komentar..." required>{{ $v->content }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer bg-secondary mt-2">
+                            <a type="button" class="btn btn-danger btn-sm btncancel text-light" data-bs-dismiss="modal">
+                            <i class="bi bi-person-x me-1"></i> Batal</a>
+                            <button type="submit" class="btn btn-primary btn-sm btnacc text-light">
+                            <i class="bi bi-person-check me-1"></i> Setuju</button>
+                        </div>
+                    </form>                    
+                </div>
+            </div>
+        </div>
+    </div>
 
 <div class="modal fade modalmenu" id="ModalDeleteArticle-{{ $v->id }}" tabindex="-1" aria-labelledby="ModalDeleteArticleLabel" aria-hidden="true">
     <div class="modal-dialog">
